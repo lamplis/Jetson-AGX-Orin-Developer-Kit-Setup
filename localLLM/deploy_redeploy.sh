d96941a71e95c15d8ce56198304be335334e0340
lamplis <lamplis@users.noreply.github.com>
2025-04-20 12:43:08 +0200

    Update deploy_redeploy.sh

--- localLLM/deploy_redeploy.sh
+++ localLLM/deploy_redeploy.sh
@@ -3,5 +3,10 @@
 # --force-recreate	Recrée tous les conteneurs, même si rien n’a changé
 # --always-recreate-deps	Recrée aussi les dépendances indirectes (volumes, réseaux liés, etc.)
 
+# Require Hugging Face Token
+echo 'export HF_TOKEN=hf_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' >> ~/.bashrc && source ~/.bashrc
 
-docker compose pull && docker compose up -d --force-recreate
+docker compose up -d
+
+# Force pull now included in docker compose file
+# docker compose pull && docker compose up -d --force-recreate
